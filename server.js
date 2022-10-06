e78061cf0352ce17b68b574da593bd9f4fcc3794
abdelqayyim <abdelymaster@gmail.com>
2022-10-06 13:18:31 -0400

    INCREASED request accepted's capacities

--- server.js
+++ server.js
@@ -17,8 +17,17 @@ let db;
 app.locals.db = db;
 
 // MIDDLEWARE
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(cors({
+//   origin: 'http://localhost:8000'
+// }))
+// app.use(
+//   cors({
+//     origin: '*' //everywhere
+//   })
+// );
+// fixing "413 Request Entity Too Large" errors
+app.use(express.json({limit: "10mb", extended: true}))
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
 app.use((req,_,next)=> {
     console.log(`${req.method}: ${req.url}`);
     if (Object.keys(req.body).length > 0){
@@ -27,12 +36,11 @@ app.use((req,_,next)=> {
     }
     next();
 });
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 app.use(cors());
+// app.use(express.bodyParser({limit: 52428800}))
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
